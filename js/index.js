5214430720ce1cf4d429f9b0d1e3076daa83f8f6
LilyMart7 <lilymart7@gmail.com>
2019-04-09 20:53:16 +0300

    updated request

--- js/index.js
+++ js/index.js
@@ -2,24 +2,45 @@ window.addEventListener("load", Init);
 
 function Init() {
     console.log("Init");
-    var urlSport = "https://newsapi.org/v2/top-headlines?country=au&category=sports&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
+    var apiKey = "ac4c1819fc814b4da5f6a7d81ecc1596";
+
+    var category = ["sport", "entertainment", "science", "business", "technology"];
+
+    var callbackFunction = [
+        { news: sportNews },
+        { news: entertainmentNews },
+        { news: scienceNews },
+        { news: businessNews },
+        { news: technologyNews }
+    
+    ];
+      
+    callbackFunction[0].news;
+    
+   /* var urlSport = "https://newsapi.org/v2/top-headlines?country=au&category=sports&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
     var urlEntertainment = "https://newsapi.org/v2/top-headlines?country=au&category=entertainment&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
     var urlScience = "https://newsapi.org/v2/top-headlines?country=au&category=science&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
     var urlBusiness = "https://newsapi.org/v2/top-headlines?country=au&category=business&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
     var urlTechnology = "https://newsapi.org/v2/top-headlines?country=au&category=technology&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
+   
     Request(urlSport, sportNews);
     Request(urlEntertainment, entertainmentNews);
     Request(urlScience, scienceNews);
     Request(urlBusiness, businessNews);
     Request(urlTechnology, technologyNews);
+*/
 
-
+for (var i = 0; i < category.length; i++)
+//console.log(category[i]+"News");
+Request(category[i], apiKey, callbackFunction[i].news);
 
 }
 
-function Request(url, callback) {
-    var xhr = new XMLHttpRequest();
+function Request(category, apiKey, callback) {
+    
+var url = `https://newsapi.org/v2/top-headlines?country=au&category=${category}&apiKey=${apiKey}`;
 
+var xhr = new XMLHttpRequest();
 
     xhr.open("GET", url, true);
 
@@ -34,29 +55,29 @@ function Request(url, callback) {
             console.log(errStatus + ": " + errText);
         } else {
             var data = JSON.parse(xhr.responseText);
+           // console.log(data);  
             callback(data);
         }
     };
 }
 
 function sportNews(news) {
-    console.log(news);
+    console.log("sportNews", news);
 }
 
-
 function entertainmentNews(news) {
-    console.log(news);
+    console.log("entertainmentNews", news);
 }
 
 function scienceNews(news) {
-    console.log(news);
+    console.log("scienceNews", news);
 }
 
 function businessNews(news) {
-    console.log(news);
+    console.log("businessNews", news);
 }
 
 function technologyNews(news) {
-    console.log(news);
+    console.log("technologyNews", news);
 }
 
