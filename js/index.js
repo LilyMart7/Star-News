0c222e7e2c1d6db2909aa3f638705319357b0108
LilyMart7 <lilymart7@gmail.com>
2019-04-11 18:23:19 +0300

    display news in HTML blocks

--- js/index.js
+++ js/index.js
@@ -12,35 +12,35 @@ function Init() {
         { news: scienceNews },
         { news: businessNews },
         { news: technologyNews }
-    
-    ];
-      
-    callbackFunction[0].news;
-    
-   /* var urlSport = "https://newsapi.org/v2/top-headlines?country=au&category=sports&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
-    var urlEntertainment = "https://newsapi.org/v2/top-headlines?country=au&category=entertainment&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
-    var urlScience = "https://newsapi.org/v2/top-headlines?country=au&category=science&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
-    var urlBusiness = "https://newsapi.org/v2/top-headlines?country=au&category=business&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
-    var urlTechnology = "https://newsapi.org/v2/top-headlines?country=au&category=technology&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
-   
-    Request(urlSport, sportNews);
-    Request(urlEntertainment, entertainmentNews);
-    Request(urlScience, scienceNews);
-    Request(urlBusiness, businessNews);
-    Request(urlTechnology, technologyNews);
-*/
 
-for (var i = 0; i < category.length; i++)
-//console.log(category[i]+"News");
-Request(category[i], apiKey, callbackFunction[i].news);
+    ];
+
+    callbackFunction[0].news;
+
+    /* var urlSport = "https://newsapi.org/v2/top-headlines?country=au&category=sports&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
+     var urlEntertainment = "https://newsapi.org/v2/top-headlines?country=au&category=entertainment&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
+     var urlScience = "https://newsapi.org/v2/top-headlines?country=au&category=science&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
+     var urlBusiness = "https://newsapi.org/v2/top-headlines?country=au&category=business&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
+     var urlTechnology = "https://newsapi.org/v2/top-headlines?country=au&category=technology&apiKey=ac4c1819fc814b4da5f6a7d81ecc1596";
+    
+     Request(urlSport, sportNews);
+     Request(urlEntertainment, entertainmentNews);
+     Request(urlScience, scienceNews);
+     Request(urlBusiness, businessNews);
+     Request(urlTechnology, technologyNews);
+ */
+
+    for (var i = 0; i < category.length; i++)
+        //console.log(category[i]+"News");
+        Request(category[i], apiKey, callbackFunction[i].news);
 
 }
 
 function Request(category, apiKey, callback) {
-    
-var url = `https://newsapi.org/v2/top-headlines?country=au&category=${category}&apiKey=${apiKey}`;
 
-var xhr = new XMLHttpRequest();
+    var url = `https://newsapi.org/v2/top-headlines?country=au&category=${category}&apiKey=${apiKey}`;
+
+    var xhr = new XMLHttpRequest();
 
     xhr.open("GET", url, true);
 
@@ -55,7 +55,7 @@ var xhr = new XMLHttpRequest();
             console.log(errStatus + ": " + errText);
         } else {
             var data = JSON.parse(xhr.responseText);
-           // console.log(data);  
+            // console.log(data);  
             callback(data);
         }
     };
@@ -63,21 +63,114 @@ var xhr = new XMLHttpRequest();
 
 function sportNews(news) {
     console.log("sportNews", news);
+    var sport = document.querySelector("#sport");
+    var a = document.createElement("a");
+    a.innerText = news.articles[0].title;
+    a.className = "sport_news";
+    a.setAttribute("target", "_blank");
+    a.setAttribute("href", news.articles[0].url);
+    sport.appendChild(a);
+
+    for (var i = 0; i < 4; i++) {
+        a.innerText = news.articles[i].title;
+        a.setAttribute("href", news.articles[i].url);
+        a.setAttribute("target", "_blank");
+        a.className = "sport_news";
+        var p = document.createElement("p");
+        var a = document.createElement("a");
+        sport.appendChild(p);
+        p.appendChild(a);
+    }
 }
 
 function entertainmentNews(news) {
     console.log("entertainmentNews", news);
+    var entertainment = document.querySelector("#entertainment");
+    var a = document.createElement("a");
+    a.innerText = news.articles[1].title;
+    a.className = "entertainment_news";
+    a.setAttribute("target", "_blank");
+    a.setAttribute("href", news.articles[1].url);
+    entertainment.appendChild(a);
+
+    for (var i = 0; i < 4; i++) {
+        a.innerText = news.articles[i].title;
+        a.setAttribute("href", news.articles[i].url);
+        a.setAttribute("target", "_blank");
+        a.className = "entertainment_news";
+        var p = document.createElement("p");
+        var a = document.createElement("a");
+        entertainment.appendChild(p);
+        p.appendChild(a);
+    }
 }
 
 function scienceNews(news) {
     console.log("scienceNews", news);
+    var science = document.querySelector("#science");
+    var a = document.createElement("a");
+    a.innerText = news.articles[2].title;
+    a.className = "science_news";
+    a.setAttribute("target", "_blank");
+    a.setAttribute("href", news.articles[2].url);
+    science.appendChild(a);
+
+    for (var i = 0; i < 4; i++) {
+        a.innerText = news.articles[i].title;
+        a.setAttribute("href", news.articles[i].url);
+        a.setAttribute("target", "_blank");
+        a.className = "science_news";
+        var p = document.createElement("p");
+        var a = document.createElement("a");
+        science.appendChild(p);
+        p.appendChild(a);
+    }
 }
 
 function businessNews(news) {
     console.log("businessNews", news);
+    var business = document.querySelector("#business");
+    var a = document.createElement("a");
+    a.innerText = news.articles[3].title;
+    a.className = "business_news";
+    a.setAttribute("target", "_blank");
+    a.setAttribute("href", news.articles[3].url);
+    business.appendChild(a);
+
+    for (var i = 0; i < 4; i++) {
+        a.innerText = news.articles[i].title;
+        a.setAttribute("href", news.articles[i].url);
+        a.setAttribute("target", "_blank");
+        a.className = "business_news";
+        var p = document.createElement("p");
+        var a = document.createElement("a");
+        business.appendChild(p);
+        p.appendChild(a);
+    }
 }
 
 function technologyNews(news) {
     console.log("technologyNews", news);
+    var technology = document.querySelector("#technology");
+    var a = document.createElement("a");
+    a.innerText = news.articles[4].title;
+    a.className = "technology_news";
+    a.setAttribute("target", "_blank");
+    a.setAttribute("href", news.articles[4].url);
+    technology.appendChild(a);
+
+    for (var i = 0; i < 4; i++) {
+        a.innerText = news.articles[i].title;
+        a.setAttribute("href", news.articles[i].url);
+        a.setAttribute("target", "_blank");
+        a.className = "technology_news";
+        var p = document.createElement("p");
+        var a = document.createElement("a");
+        technology.appendChild(p);
+        p.appendChild(a);
+    }
 }
+
+
+
 
